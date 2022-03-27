8be170fbc52eee34350ef2cf97f402246e093089
Julian Kędys <juliankedys01@gmail.com>
2022-03-27 20:55:02 +0200

    Program.java

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,7 +1,9 @@
 //Some comment
 class HelloWorld {
 	public static void main(String[] args) {
+		System.out.println("New change 1");
 		System.out.println("Hello, Worldo!");
+		System.out.println("New change 2");
 		System.out.println("New change :oo");
 		System.out.println("New change 3");
 	}
