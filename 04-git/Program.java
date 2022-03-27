f746eff5108c01d0d8ea46534e0aae221f30a9e5
Julian Kędys <juliankedys01@gmail.com>
2022-03-27 20:55:51 +0200

    First change

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -1,6 +1,8 @@
+//Some comment
 class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, Worldo!");
 		System.out.println("New change :oo");
+		System.out.println("New change 3");
 	}
 }
